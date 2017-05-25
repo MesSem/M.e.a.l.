a67fcbbe9ccf1c0bd36b9050b78f83c557151241
Simone <morettinisimo96@gmail.com>
2017-05-25 17:10:20 +0200

    Creazione struttura file e cartelle

---
+++ app/app.config.js
@@ -0,0 +1,23 @@
+/*
+angular.
+  module('studentApp').
+  config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/students', {
+          template: '<student-list></student-list>'
+        }).
+        when('/students/:studentId', {
+          template: '<student-detail></student-detail>'
+        }).
+        when('/add/students', {
+          template: '<add-student></add-student>'
+        }).
+        when('/edit/students/:studentId', {
+          template: '<edit-student></edit-student>'
+        }).
+        otherwise('/students');
+    }
+]);*/
---
+++ app/app.module.js
@@ -0,0 +1,10 @@
+
+// Define the `studentApp` module
+var studentApp = angular.module('studentApp', [
+  'ngRoute',
+  'studentList',
+  'studentDetail',
+  'addStudent',
+  'editStudent',
+  'common'
+]);
