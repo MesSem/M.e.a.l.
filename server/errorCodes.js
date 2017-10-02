66ae2b338cf60bbf82de08c6e968eda43818cc74
Simone <morettinisimo96@gmail.com>
2017-10-02 09:30:06 +0200

    Creazione funzione per la gestione degli errori

--- server/errorCodes.js
+++ server/errorCodes.js
@@ -1,3 +1,6 @@
+var cfg = require("./config.js");
+var toJSON = require( 'utils-error-to-json' );
+
 var errorCodes = this;
 
 // esporto api_utilities così posso utilizzare i suoi metodi e attributi,
@@ -13,3 +16,29 @@ this.ERR_API_WRONG_PSW = 'ERR_API_WRONG_PSW';
 this.ERR_API_UNAUTHORIZED= 'ERR_API_UNAUTHORIZED';
 this.ERR_JWT_TOKEN_NOT_FOUND= 'ERR_JWT_TOKEN_NOT_FOUND';
 this.ERR_TOKEN_EXPIRED= 'ERR_TOKEN_EXPIRED';
+
+/**
+ * Function that send error infomation to client
+ * @param  {[type]} res
+ * @param  {[type]} cod      Code of the error, choose one from errorCodes.js
+ * @param  {[type]} message  Message of the error
+ * @param  {[type]} moreInfo More info of the error, only in developer mode. It can be everything. This method will convert it in json
+ * @param  {[type]} status   Status of the error
+ * @return {[type]}          res
+ */
+this.sendError= function(res,cod, message, moreInfo, status){
+  if(status==undefined){
+    status=500;
+  }
+  if(cfg.mode!='dev'){
+    moreInfo=undefined;
+  }else {
+    errInfo=toJSON(moreInfo);
+  }
+  res.status(status).json({
+    success: false,
+    code:cod,
+    message: message,
+    error:errInfo
+  });
+}
